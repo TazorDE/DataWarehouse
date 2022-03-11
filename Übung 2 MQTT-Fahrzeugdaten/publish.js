afcd8ba77ff6a0ec386462faea12c1d1105271ba
Nils Malte Teichert <Nils.Teichert@ibm.com>
2022-03-11 13:06:46 +0100

    Verbessere Zeitgenerierung

--- Übung 2 MQTT-Fahrzeugdaten/publish.js
+++ Übung 2 MQTT-Fahrzeugdaten/publish.js
@@ -10,14 +10,41 @@ client.on("connect", function () {
     cron.schedule("*/5 * * * * *", function () {
         let data = generateData();
         console.log(data);
-        client.publish("DataMgmt/FIN", JSON.stringify(data));
+        //client.publish("DataMgmt/FIN", JSON.stringify(data));
     });
 });
 
 function generateData() {
     // format time as DD.MM.YYYY HH24:MI:SS.MS
     let date = new Date();
-    let time = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "." + date.getMilliseconds();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let milliseconds = date.getMilliseconds();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (milliseconds < 10) {
+        milliseconds = "00" + milliseconds;
+    } else if (milliseconds < 100) {
+        milliseconds = "0" + milliseconds;
+    }
+    let time = day + "." + month + "." + year + " " + hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 
     return {
         fin: "WVWIAmVeryRandom",
